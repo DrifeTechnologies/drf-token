6ef4371fbc7ae7e8935e618ab2254643b8b86dc4
Mudit Marda <muditmarda109@gmail.com>
2021-08-03 11:53:24 +0530

    Add basic BEP20 contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var DrifeERCToken = artifacts.require("./DrifeERCToken.sol");
+var DrifeBEPToken = artifacts.require("./DrifeBEPToken.sol");
 
 module.exports = async function (deployer) {
   await deployer.deploy(DrifeERCToken);
+  await deployer.deploy(DrifeBEPToken);
 };
